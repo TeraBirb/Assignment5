6156e99539d6df4f43e03a62c121417a4dd0d4e7
Isaiah <isaiah.g.callano@gmail.com>
2021-03-11 10:03:11 -0800

    first commit

---
+++ Assignment 5/Assignment 5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // 1. Vowels and consonants
+
+
+    // 2. Finonacci numbers
+
+
+    // 3. Interest and Mortgage Payment
+}
+
